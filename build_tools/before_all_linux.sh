b5b1e2b1fb5867ff06ea5044d4e31646314f242f
Robert McArthur <u6956078@anu.edu.au>
2024-06-19 04:40:12 +0000

    DEV: support alpine linux

--- build_tools/before_all_linux.sh
+++ build_tools/before_all_linux.sh
@@ -2,6 +2,8 @@ if command -v apt-get &> /dev/null; then
 apt-get update -y && apt-get install -y libeigen3-dev libboost-all-dev
 elif command -v yum &> /dev/null; then
 yum update -y && yum install -y eigen3-devel boost-devel
+elif command -v apk &> /dev/null; then
+apk update -y && apk add -y eigen-dev boost-dev
 else
 echo "Unsupported package manager";
 exit 1;
