1ce05682d5385ac42013b5c5db55c3db4644451b
Robert McArthur <u6956078@anu.edu.au>
2024-06-24 04:39:54 +0000

    DEV: attempt serial build when making wheels

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -1,7 +1,8 @@
 cd iqtree2
 git apply ../fpic-iqtree.patch
+rm -rf build
 mkdir build && cd build
 cmake -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
-make -j
+make
 cd ../..
 mv iqtree2/build/libiqtree2.a piqtree2/libiqtree/
