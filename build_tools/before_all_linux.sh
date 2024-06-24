8441d019f8e27831d02ea6a2bc04a69431e58c1c
Robert McArthur <u6956078@anu.edu.au>
2024-06-24 05:43:15 +0000

    DEV: drop ARM linux support and attempt llvm dependency

--- build_tools/before_all_linux.sh
+++ build_tools/before_all_linux.sh
@@ -1,9 +1,10 @@
 if command -v apt-get &> /dev/null; then
 apt-get update -y && apt-get install -y libeigen3-dev libboost-all-dev
+apt install llvm
 elif command -v yum &> /dev/null; then
-yum update -y && yum install -y eigen3-devel boost-devel
+yum update -y && yum install -y eigen3-devel boost-devel llvm-toolset-7
 elif command -v apk &> /dev/null; then
-apk update && apk add eigen-dev boost-dev
+apk update && apk add eigen-dev boost-dev llvm15
 else
 echo "Unsupported package manager";
 exit 1;
