48346aaf8017b620ba2a910f572fbb090547f762
Robert McArthur <u6956078@anu.edu.au>
2024-06-24 05:44:07 +0000

    DEV: include parallel build once more

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -3,6 +3,6 @@ git apply ../fpic-iqtree.patch
 rm -rf build
 mkdir build && cd build
 cmake -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
-make
+make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/libiqtree/
