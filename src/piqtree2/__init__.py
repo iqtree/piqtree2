ab8b49a61e5b4b861eb2f3ddb59a13286c371417
Robert McArthur <u6956078@anu.edu.au>
2024-06-28 06:24:22 +0000

    DEV: implement all functions ( and  seg fault on multiple calls)

--- src/piqtree2/__init__.py
+++ src/piqtree2/__init__.py
@@ -1,3 +1,7 @@
-from _piqtree2 import *
+"""piqtree2 - access the power of IQ-TREE within Python."""
+
+from ._iq_wrappers import build_tree, fit_tree, random_trees, robinson_foulds
 
 __version__ = "0.0.1.dev0"
+
+__all__ = ["build_tree", "fit_tree", "random_trees", "robinson_foulds"]
---
+++ src/piqtree2/_iq_wrappers.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+
+import cogent3
+import numpy as np
+import yaml
+from _piqtree2 import iq_build_tree, iq_fit_tree, iq_random_tree, iq_robinson_fould
+
+
+def robinson_foulds(*trees: cogent3.PhyloNode) -> np.ndarray:
+    pairwise_distances = np.zeros((len(trees), len(trees)))
+    for i in range(1, len(trees)):
+        for j in range(i):
+            rf = iq_robinson_fould(str(trees[i]), str(trees[j]))
+            pairwise_distances[i, j] = rf
+            pairwise_distances[j, i] = rf
+    return pairwise_distances
+
+
+class TreeGenMode(Enum):
+    YULE_HARDING = auto()
+    UNIFORM = auto()
+    CATERPILLAR = auto()
+    BALANCED = auto()
+    BIRTH_DEATH = auto()
+    STAR_TREE = auto()
+
+
+def random_trees(
+    num_taxa: int,
+    tree_mode: TreeGenMode,
+    num_trees: int,
+    rand_seed: int | None = None,
+) -> tuple[cogent3.PhyloNode]:
+    if rand_seed is None:
+        rand_seed = 0  # The default rand_seed in IQ-TREE
+    trees = iq_random_tree(num_taxa, tree_mode.name, num_trees, rand_seed)
+    return tuple(
+        cogent3.make_tree(newick + ";") for newick in trees.split(";") if newick != ""
+    )
+
+
+def build_tree(
+    aln: cogent3.Alignment, model: str, rand_seed: int | None = None
+) -> cogent3.PhyloNode:
+    if rand_seed is None:
+        rand_seed = 0  # The default rand_seed in IQ-TREE
+
+    names = aln.names
+    seqs = [str(seq) for seq in aln.iter_seqs(names)]
+
+    yaml_result = yaml.safe_load(iq_build_tree(names, seqs, model, rand_seed))
+    return cogent3.make_tree(yaml_result["PhyloTree"]["newick"])
+
+
+def fit_tree(
+    aln: cogent3.Alignment,
+    tree: cogent3.PhyloNode,
+    model: str,
+    rand_seed: int | None = None,
+) -> cogent3.PhyloNode:
+    if rand_seed is None:
+        rand_seed = 0  # The default rand_seed in IQ-TREE
+
+    names = aln.names
+    seqs = [str(seq) for seq in aln.iter_seqs(names)]
+    newick = str(tree)
+
+    yaml_result = yaml.safe_load(iq_fit_tree(names, seqs, model, newick, rand_seed))
+    return cogent3.make_tree(yaml_result["PhyloTree"]["newick"])
