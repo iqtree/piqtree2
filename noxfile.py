6e2e54d581dd867f95e409f70da861dd953ad72c
Robert McArthur <u6956078@anu.edu.au>
2024-07-04 04:53:36 +0000

    DEV: add ci

---
+++ noxfile.py
@@ -0,0 +1,15 @@
+import os
+
+import nox
+
+_py_versions = range(9, 13)
+
+
+@nox.session(python=[f"3.{v}" for v in _py_versions])
+def test(session):
+    posargs = list(session.posargs)
+    env = os.environ.copy()
+
+    install_spec = "-e.[dev]"
+    session.install(install_spec)
+    session.run("pytest", *posargs, env=env)
