9cb6375dfad97a65fc86780cf02fd92338fc9f7c
Robert McArthur <u6956078@anu.edu.au>
2024-07-30 12:11:09 +0000

    DEV: fix ci pathing under new repo structure

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -4,4 +4,4 @@ mkdir build && cd build
 cmake -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
 make -j
 cd ../..
-mv iqtree2/build/libiqtree2.a src/piqtree2/libiqtree/
+mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
