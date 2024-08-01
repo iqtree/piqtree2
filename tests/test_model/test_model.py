b0f706adfb1286e9c8f2b9943615675792efe15e
Robert McArthur <u6956078@anu.edu.au>
2024-08-01 06:29:45 +0000

    TST: extend tests for model and options

---
+++ tests/test_model/test_model.py
@@ -0,0 +1,25 @@
+import pytest
+from piqtree2.model import AaModel, DnaModel
+
+
+@pytest.mark.parametrize("model_class", [DnaModel, AaModel])
+def test_number_of_descriptions(model_class):
+    assert len(model_class) == len(model_class._descriptions())
+
+
+@pytest.mark.parametrize("model_class", [DnaModel, AaModel])
+def test_descriptions_exist(model_class):
+    for model in model_class:
+        # Raises an error if description not present
+        _ = model.description
+
+
+@pytest.mark.parametrize(
+    ("model_class", "model_type"),
+    [(DnaModel, "nucleotide"), (AaModel, "protein")],
+)
+def test_model_type(model_class, model_type):
+    assert model_class.model_type() == model_type
+
+    for model in model_class:
+        assert model.model_type() == model_type
--- tests/test_model/test_options.py
+++ tests/test_model/test_options.py
@@ -1,9 +1,32 @@
 # testing the display of functions
 import pytest
 from piqtree2 import available_models
+from piqtree2.model import AaModel, DnaModel
 
 
-@pytest.mark.parametrize("model_type", [None, "dna", "protein"])
-def test_available_models(model_type):
+@pytest.mark.parametrize(
+    ("model_class", "model_type"),
+    [(None, None), (DnaModel, "dna"), (AaModel, "protein")],
+)
+def test_num_available_models(model_class, model_type):
     table = available_models(model_type)
-    assert table.shape[0] > 0
+    total_models = (
+        len(DnaModel) + len(AaModel) if model_class is None else len(model_class)
+    )
+    assert total_models > 0
+    assert table.shape[0] == total_models
+
+
+@pytest.mark.parametrize(
+    ("model_fetch", "model_type"),
+    [(None, None), ("dna", "nucleotide"), ("protein", "protein")],
+)
+def test_available_models_types(model_fetch, model_type):
+    table = available_models(model_fetch)
+
+    if model_type is None:
+        for check_model_type in table[:, 0]:
+            assert check_model_type[0] in ["nucleotide", "protein"]
+    else:
+        for check_model_type in table[:, 0]:
+            assert check_model_type[0] == model_type
