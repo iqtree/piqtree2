98d1ddca985a7d1f39c95ba1fb006ad081de3529
rmcar17 <robert.mcarthur@anu.edu.au>
2024-08-07 04:33:02 +0000

    DEV: add freq type enum

--- src/piqtree2/model/__init__.py
+++ src/piqtree2/model/__init__.py
@@ -1,6 +1,7 @@
 """Models available in IQ-TREE."""
 
+from ._freq_type import FreqType
 from ._model import AaModel, DnaModel, SubstitutionModel
 from ._options import available_models
 
-__all__ = ["available_models", "AaModel", "DnaModel", "SubstitutionModel"]
+__all__ = ["available_models", "AaModel", "DnaModel", "FreqType", "SubstitutionModel"]
