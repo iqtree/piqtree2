5d17fe7fbf4d90b8901d501beebab72792ca825f
rmcar17 <robert.mcarthur@anu.edu.au>
2024-08-29 04:00:22 +0000

    TST: test whether piqtree2 apps are pickleable

---
+++ tests/test_app/test_pickle.py
@@ -0,0 +1,11 @@
+"""Apps must be pickleable to be able to be run with parallel=True"""
+
+import pickle
+
+from cogent3 import get_app
+from piqtree2._app import _ALL_APP_NAMES
+
+
+def test_pickle():
+    for app_name in _ALL_APP_NAMES:
+        assert len(pickle.dumps(get_app(app_name))) > 0
