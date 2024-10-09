72fb7f1294628b4ea83cd3d916e6678e1c899d5d
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-09 04:35:44 +0000

    TST: test neighbour joining tree

---
+++ tests/test_iqtree/test_nj_tree.py
@@ -0,0 +1,12 @@
+from cogent3 import ArrayAlignment, make_tree
+
+from piqtree2 import jc_distances, nj_tree
+
+
+def test_nj_tree(five_otu: ArrayAlignment) -> None:
+    expected = make_tree("(((Human, Chimpanzee), Rhesus), Manatee, Dugong);")
+
+    dists = jc_distances(five_otu)
+    actual = nj_tree(dists)
+
+    assert expected.same_topology(actual)
