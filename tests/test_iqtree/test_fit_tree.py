f90033531c941cb6e1026100aff0ea795fb0cd1a
Yapeng Lang <u7181074@gmail.com>
2024-10-16 12:17:31 +0000

    TST: refactored the test_fit_tree for clear function interface

--- tests/test_iqtree/test_fit_tree.py
+++ tests/test_iqtree/test_fit_tree.py
@@ -1,34 +1,41 @@
 import pytest
 from cogent3 import ArrayAlignment, get_app, make_tree
+from cogent3.app.result import model_result
+from cogent3.core.tree import PhyloNode
 
 import piqtree2
 from piqtree2.model import DnaModel, Model
 
 
-def check_likelihood(got:float, expected:float) -> None:
-    assert got == pytest.approx(expected)
+def check_likelihood(got: PhyloNode, expected: model_result) -> None:
+    assert got.params["lnL"] == pytest.approx(expected.lnL)
 
-def check_motif_probs(got:dict, expected:dict) -> None:
+
+def check_motif_probs(got: PhyloNode, expected: PhyloNode) -> None:
+    expected = expected.params["mprobs"]
+    got = got.params["mprobs"]
+
     expected_keys = set(expected.keys())
     got_keys = set(got.keys())
+
     # Check that the base characters are the same
     assert expected_keys == got_keys
 
     # Check that the probs are the same
     expected_values = [expected[key] for key in expected_keys]
     got_values = [got[key] for key in expected_keys]
-    assert all(got == pytest.approx(exp) for got, exp in zip(got_values, expected_values))
+    assert all(
+        got == pytest.approx(exp) for got, exp in zip(got_values, expected_values)
+    )
 
-def check_rate_parameters(got:dict, expected:dict) -> None:
+
+def check_rate_parameters(got: PhyloNode, expected: PhyloNode) -> None:
     # Collect all rate parameters in got and expected
     exclude = {"length", "ENS", "paralinear", "mprobs"}
     expected_keys = {
-        k
-        for k in expected[0].params
-        if k not in exclude
+        k for k in expected.get_edge_vector()[0].params if k not in exclude
     }
-    got_keys = {k for k in got[0].params if k not in exclude}
-
+    got_keys = {k for k in got.get_edge_vector()[0].params if k not in exclude}
 
     # Check that the keys of rate are the same
     assert expected_keys == got_keys
@@ -37,9 +44,15 @@ def check_rate_parameters(got:dict, expected:dict) -> None:
     expected_values = [expected[0].params[key] for key in expected_keys]
     got_values = [got[0].params[key] for key in expected_keys]
 
-    assert all(got == pytest.approx(exp, rel=1e-2) for got, exp in zip(got_values, expected_values))
+    assert all(
+        got == pytest.approx(exp, rel=1e-2)
+        for got, exp in zip(got_values, expected_values)
+    )
 
-def check_branch_lengths(got:dict, expected:dict) -> None:
+
+def check_branch_lengths(got: PhyloNode, expected: PhyloNode) -> None:
+    got = got.get_distances()
+    expected = expected.get_distances()
     # Check that the keys of branch lengths are the same
     assert got.keys() == expected.keys()
 
@@ -47,7 +60,11 @@ def check_branch_lengths(got:dict, expected:dict) -> None:
     expected_values = [expected[key] for key in expected]
     got_values = [got[key] for key in expected]
 
-    assert all(got == pytest.approx(exp, rel=1e-2) for got, exp in zip(got_values, expected_values))
+    assert all(
+        got == pytest.approx(exp, rel=1e-2)
+        for got, exp in zip(got_values, expected_values)
+    )
+
 
 @pytest.mark.parametrize(
     ("iq_model", "c3_model"),
@@ -66,14 +83,14 @@ def test_fit_tree(three_otu: ArrayAlignment, iq_model: DnaModel, c3_model: str) 
     expected = app(three_otu)
 
     got1 = piqtree2.fit_tree(three_otu, tree_topology, Model(iq_model), rand_seed=1)
-    check_likelihood(got1.params["lnL"], expected.lnL)
-    check_motif_probs(got1.params["mprobs"], expected.tree.params["mprobs"])
-    check_rate_parameters(got1.get_edge_vector(), expected.tree.get_edge_vector())
-    check_branch_lengths(got1.get_distances(), expected.tree.get_distances())
+    check_likelihood(got1, expected)
+    check_motif_probs(got1, expected.tree)
+    check_rate_parameters(got1, expected.tree)
+    check_branch_lengths(got1, expected.tree)
 
     # Should be within an approximation for any seed
     got2 = piqtree2.fit_tree(three_otu, tree_topology, Model(iq_model), rand_seed=None)
-    check_likelihood(got2.params["lnL"], expected.lnL)
-    check_motif_probs(got2.params["mprobs"], expected.tree.params["mprobs"])
-    check_rate_parameters(got2.get_edge_vector(), expected.tree.get_edge_vector())
-    check_branch_lengths(got2.get_distances(), expected.tree.get_distances())
+    check_likelihood(got2, expected)
+    check_motif_probs(got2, expected.tree)
+    check_rate_parameters(got2, expected.tree)
+    check_branch_lengths(got2, expected.tree)
