262446b5027addc1c60256a2e0b6003e87878061
Yapeng Lang <u7181074@gmail.com>
2024-10-16 06:51:28 +0000

    TST: added tests for fit_tree for branch lenths, and wrapped check for different params in functions

--- tests/test_iqtree/test_fit_tree.py
+++ tests/test_iqtree/test_fit_tree.py
@@ -1,8 +1,53 @@
-import piqtree2
 import pytest
 from cogent3 import ArrayAlignment, get_app, make_tree
+
+import piqtree2
 from piqtree2.model import DnaModel, Model
 
+
+def check_likelihood(got:float, expected:float) -> None:
+    assert got == pytest.approx(expected)
+
+def check_motif_probs(got:dict, expected:dict) -> None:
+    expected_keys = set(expected.keys())
+    got_keys = set(got.keys())
+    # Check that the base characters are the same
+    assert expected_keys == got_keys
+
+    # Check that the probs are the same
+    expected_values = [expected[key] for key in expected_keys]
+    got_values = [got[key] for key in expected_keys]
+    assert all(got == pytest.approx(exp) for got, exp in zip(got_values, expected_values))
+
+def check_rate_parameters(got:dict, expected:dict) -> None:
+    # Collect all rate parameters in got and expected
+    exclude = {"length", "ENS", "paralinear", "mprobs"}
+    expected_keys = {
+        k
+        for k in expected[0].params
+        if k not in exclude
+    }
+    got_keys = {k for k in got[0].params if k not in exclude}
+
+
+    # Check that the keys of rate are the same
+    assert expected_keys == got_keys
+
+    # Check that the values of rate are the same
+    expected_values = [expected[0].params[key] for key in expected_keys]
+    got_values = [got[0].params[key] for key in expected_keys]
+
+    assert all(got == pytest.approx(exp, rel=1e-2) for got, exp in zip(got_values, expected_values))
+
+def check_branch_lengths(got:dict, expected:dict) -> None:
+    # Check that the keys of branch lengths are the same
+    assert got.keys() == expected.keys()
+
+    # Check that the branch lengths are the same
+    expected_values = [expected[key] for key in expected]
+    got_values = [got[key] for key in expected]
+
+    assert all(got == pytest.approx(exp, rel=1e-2) for got, exp in zip(got_values, expected_values))
 
 @pytest.mark.parametrize(
     ("iq_model", "c3_model"),
@@ -21,27 +66,14 @@ def test_fit_tree(three_otu: ArrayAlignment, iq_model: DnaModel, c3_model: str) 
     expected = app(three_otu)
 
     got1 = piqtree2.fit_tree(three_otu, tree_topology, Model(iq_model), rand_seed=1)
-    assert got1.params["lnL"] == pytest.approx(expected.lnL)
+    check_likelihood(got1.params["lnL"], expected.lnL)
+    check_motif_probs(got1.params["mprobs"], expected.tree.params["mprobs"])
+    check_rate_parameters(got1.get_edge_vector(), expected.tree.get_edge_vector())
+    check_branch_lengths(got1.get_distances(), expected.tree.get_distances())
 
     # Should be within an approximation for any seed
     got2 = piqtree2.fit_tree(three_otu, tree_topology, Model(iq_model), rand_seed=None)
-    assert got2.params["lnL"] == pytest.approx(expected.lnL)
-
-    # Check motif and rate parameters
-    expected_motif_prob = expected.tree.params["mprobs"]
-    for k, v in expected_motif_prob.items():
-        assert k in got2.params["mprobs"]
-        assert got2.params["mprobs"][k] == pytest.approx(v)
-
-    exclude = {"length", "ENS", "paralinear", "mprobs"}
-    expected_rates = {
-        k: v
-        for k, v in expected.tree.get_edge_vector()[0].params.items()
-        if k not in exclude
-    }
-    for k, v in expected_rates.items():
-        assert k in got2.get_edge_vector()[0].params
-        assert got2.get_edge_vector()[0].params[k] == pytest.approx(
-            v,
-            rel=1e-2,
-        )
+    check_likelihood(got2.params["lnL"], expected.lnL)
+    check_motif_probs(got2.params["mprobs"], expected.tree.params["mprobs"])
+    check_rate_parameters(got2.get_edge_vector(), expected.tree.get_edge_vector())
+    check_branch_lengths(got2.get_distances(), expected.tree.get_distances())
