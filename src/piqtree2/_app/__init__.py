02f582f307eb8263557913a4e29fcc889d7184d8
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-10-17 15:43:37 +1100

    MAINT: change app module to importing whole composable module
    [CHANGED] this allows us to define different app types

--- src/piqtree2/_app/__init__.py
+++ src/piqtree2/_app/__init__.py
@@ -4,24 +4,32 @@ from typing import Optional, Union
 
 import cogent3
 import cogent3.app.typing as c3_types
-from cogent3.app.composable import define_app
+from cogent3.app import composable
 from cogent3.util.misc import extend_docstring_from
 
 from piqtree2 import TreeGenMode, build_tree, fit_tree, nj_tree, random_trees
 from piqtree2.model import Model
 
 
-@define_app
-@extend_docstring_from(build_tree)
-def piqtree_phylo(
-    aln: Union[cogent3.Alignment, cogent3.ArrayAlignment],
-    model: Model,
-    rand_seed: Optional[int] = None,
-) -> Union[cogent3.PhyloNode, cogent3.app.typing.SerialisableType]:
-    return build_tree(aln, model, rand_seed)
+@composable.define_app
+class piqtree_phylo:
+    @extend_docstring_from(build_tree)
+    def __init__(
+        self,
+        model: str,
+        rand_seed: Optional[int] = None,
+    ) -> None:
+        self._model = Model(model)
+        self._rand_seed = rand_seed
+
+    def main(
+        self,
+        aln: Union[cogent3.Alignment, cogent3.ArrayAlignment],
+    ) -> Union[cogent3.PhyloNode, cogent3.app.typing.SerialisableType]:
+        return build_tree(aln, self._model, self._rand_seed)
 
 
-@define_app
+@composable.define_app
 @extend_docstring_from(fit_tree)
 def piqtree_fit(
     aln: Union[cogent3.Alignment, cogent3.ArrayAlignment],
@@ -32,7 +40,7 @@ def piqtree_fit(
     return fit_tree(aln, tree, model, rand_seed)
 
 
-@define_app
+@composable.define_app
 @extend_docstring_from(random_trees)
 def piqtree_random_trees(
     num_taxa: int,
@@ -43,7 +51,7 @@ def piqtree_random_trees(
     return random_trees(num_taxa, tree_mode, num_trees, rand_seed)
 
 
-@define_app
+@composable.define_app
 @extend_docstring_from(nj_tree)
 def piqtree_nj(dists: c3_types.PairwiseDistanceType) -> cogent3.PhyloNode:
     return nj_tree(dists)
