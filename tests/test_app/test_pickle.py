bfef7b094ba5f36d6a8ec2025437205bc0ff0465
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-10-17 15:49:51 +1100

    MAINT: changes in app module to existing
    [CHANGED] import whole composable module as this allows us to
        define different app types
    
    [CHANGED] make the piqtree_phylo a class to demo benefits
        in terms of the display of user help.
    
    [NEW] added substitution_mode.get_model() function which returns
        the enum given a string or enum. Modified Model to use this.
        This makes it easier for users to define the substitution
        models. A user should only ever have to import cogent3 to
        get everythjing they need from piqtree2.

--- tests/test_app/test_pickle.py
+++ tests/test_app/test_pickle.py
@@ -3,9 +3,15 @@
 import pickle
 
 from cogent3 import get_app
+
 from piqtree2._app import _ALL_APP_NAMES
 
 
 def test_pickle() -> None:
     for app_name in _ALL_APP_NAMES:
-        assert len(pickle.dumps(get_app(app_name))) > 0
+        app = (
+            get_app(app_name)
+            if app_name != "piqtree_phylo"
+            else get_app(app_name, "JC")
+        )
+        assert len(pickle.dumps(app)) > 0
