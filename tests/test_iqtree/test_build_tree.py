b5925623f00b0aa000f1e7d5a8e6eb21b31c91d7
Yapeng Lang <u7181074@gmail.com>
2024-10-17 04:05:23 +0000

    TST: rolled back check_build_tree

--- tests/test_iqtree/test_build_tree.py
+++ tests/test_iqtree/test_build_tree.py
@@ -15,18 +15,33 @@ from piqtree2.model import (
 )
 
 
-def init_model(
+def check_build_tree(
+    four_otu: ArrayAlignment,
     dna_model: DnaModel,
     freq_type: Optional[FreqType] = None,
     invariable_sites: Optional[bool] = None,
     rate_model: Optional[RateModel] = None,
-) -> Model:
+) -> None:
+    expected = make_tree("(Human,Chimpanzee,(Rhesus,Mouse));")
 
-    return Model(
+    model = Model(
         dna_model,
         freq_type,
         RateType(invariable_sites=invariable_sites, model=rate_model),
     )
+
+    got1 = piqtree2.build_tree(four_otu, model, rand_seed=1)
+    got1 = got1.unrooted()
+    # Check topology
+    assert expected.same_topology(got1.unrooted())
+    # Check if branch lengths exist
+    assert all("length" in v.params for v in got1.get_edge_vector())
+
+    # Should be similar for any seed
+    got2 = piqtree2.build_tree(four_otu, model, rand_seed=None)
+    got2 = got2.unrooted()
+    assert expected.same_topology(got2)
+    assert all("length" in v.params for v in got2.get_edge_vector())
 
 
 @pytest.mark.parametrize("dna_model", list(DnaModel)[:22])
@@ -36,40 +51,12 @@ def test_non_lie_build_tree(
     dna_model: DnaModel,
     freq_type: FreqType,
 ) -> None:
-    expected = make_tree("(Human,Chimpanzee,(Rhesus,Mouse));")
-    model = init_model(dna_model, freq_type)
-
-    got1 = piqtree2.build_tree(four_otu, model, rand_seed=1)
-    got1 = got1.unrooted()
-    # Check topology
-    assert expected.same_topology(got1.unrooted())
-    # Check if branch lengths exist
-    assert all("length" in v.params for v in got1.get_edge_vector())
-
-    # Should be similar for any seed
-    got2 = piqtree2.build_tree(four_otu, model, rand_seed=None)
-    got2 = got2.unrooted()
-    assert expected.same_topology(got2)
-    assert all("length" in v.params for v in got2.get_edge_vector())
+    check_build_tree(four_otu, dna_model, freq_type)
 
 
 @pytest.mark.parametrize("dna_model", list(DnaModel)[22:])
 def test_lie_build_tree(four_otu: ArrayAlignment, dna_model: DnaModel) -> None:
-    expected = make_tree("(Human,Chimpanzee,(Rhesus,Mouse));")
-    model = init_model(dna_model)
-
-    got1 = piqtree2.build_tree(four_otu, model, rand_seed=1)
-    got1 = got1.unrooted()
-    # Check topology
-    assert expected.same_topology(got1.unrooted())
-    # Check if branch lengths exist
-    assert all("length" in v.params for v in got1.get_edge_vector())
-
-    # Should be similar for any seed
-    got2 = piqtree2.build_tree(four_otu, model, rand_seed=None)
-    got2 = got2.unrooted()
-    assert expected.same_topology(got2)
-    assert all("length" in v.params for v in got2.get_edge_vector())
+    check_build_tree(four_otu, dna_model)
 
 
 @pytest.mark.parametrize("dna_model", list(DnaModel)[:5])
@@ -90,22 +77,9 @@ def test_rate_model_build_tree(
     invariable_sites: Optional[bool],
     rate_model: RateModel,
 ) -> None:
-    expected = make_tree("(Human,Chimpanzee,(Rhesus,Mouse));")
-    model = init_model(
+    check_build_tree(
+        four_otu,
         dna_model,
         invariable_sites=invariable_sites,
         rate_model=rate_model,
     )
-
-    got1 = piqtree2.build_tree(four_otu, model, rand_seed=1)
-    got1 = got1.unrooted()
-    # Check topology
-    assert expected.same_topology(got1.unrooted())
-    # Check if branch lengths exist
-    assert all("length" in v.params for v in got1.get_edge_vector())
-
-    # Should be similar for any seed
-    got2 = piqtree2.build_tree(four_otu, model, rand_seed=None)
-    got2 = got2.unrooted()
-    assert expected.same_topology(got2)
-    assert all("length" in v.params for v in got2.get_edge_vector())
