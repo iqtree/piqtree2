eeb48b116e9a23796421659e6399ea4aad9b9e22
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-15 04:14:00 +0000

    ENH: allow `Model` to be imported from top level for easy Model construction

--- src/piqtree2/__init__.py
+++ src/piqtree2/__init__.py
@@ -10,7 +10,12 @@ from piqtree2.iqtree import (
     random_trees,
     robinson_foulds,
 )
-from piqtree2.model import available_freq_type, available_models, available_rate_type
+from piqtree2.model import (
+    Model,
+    available_freq_type,
+    available_models,
+    available_rate_type,
+)
 
 __version__ = "0.3.1"
 
@@ -21,6 +26,7 @@ __all__ = [
     "build_tree",
     "fit_tree",
     "jc_distances",
+    "Model",
     "model_finder",
     "nj_tree",
     "random_trees",
