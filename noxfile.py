c23fdcb32cbd09cb6d808bd2bf52c23bcd0f1b5e
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-15 05:49:03 +0000

    DEV: add type checking github action

--- noxfile.py
+++ noxfile.py
@@ -13,3 +13,13 @@ def test(session):
     install_spec = "-e.[test]"
     session.install(install_spec)
     session.run("pytest", *posargs, env=env)
+
+
+@nox.session(python=[f"3.{v}" for v in _py_versions])
+def type_check(session):
+    posargs = list(session.posargs)
+    env = os.environ.copy()
+
+    install_spec = ".[typing]"
+    session.install(install_spec)
+    session.run("mypy", "src", "tests", *posargs, env=env)
