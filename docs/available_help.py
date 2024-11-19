fc6a929fb3bb77ddde016f6017bcf72d16d39903
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-11-20 09:37:40 +1100

    DOC: added documentation!

---
+++ docs/available_help.py
@@ -0,0 +1,37 @@
+# %% [markdown]
+# To see the apps that `piqtree2` makes available, use the cogent3 function `available_apps()` as follows.
+
+# %%
+from cogent3 import available_apps
+
+available_apps("piqtree")
+
+# %% [markdown]
+# For a particular app, use the cogent3 function `app_help()` to find out what the options are.
+
+# %%
+from cogent3 import app_help
+
+app_help("piqtree_phylo")
+
+# %%
+from cogent3 import app_help
+
+app_help("piqtree_fit")
+
+# %% [markdown]
+# ## What substitution models are available
+# You can list all the DNA substitution models `piqtree2` supports.
+
+# %%
+from cogent3 import get_app
+
+dna_models = get_app("piqtree_list_available", element_type="model")
+dna_models("dna").head(3)  # just the first 3 models
+
+# %% [markdown]
+# ## What rate heterogeneity models are available
+
+# %%
+rate_het = get_app("piqtree_list_available", element_type="rate")
+rate_het("")
---
+++ docs/phylo.py
@@ -0,0 +1,40 @@
+# %% [markdown]
+# We use the `piqtree_phylo` app to build phylogenies.
+# For this simple case, we will build a single phylogeny using the GTR model on one alignment.
+# We have a utility script for this documentation that provides the local path to that data. We will then load that data and, as it contains quite a few sequences, we will use a subset of it. We use methods on the cogent3 object to do that.
+
+# %%
+import cogent3
+
+from piqtree2 import download_dataset
+
+aln_path = download_dataset("example.phy.gz", dest_dir="data")
+aln = cogent3.load_aligned_seqs(aln_path, moltype="dna", format="phylip")
+aln
+
+
+# %% [markdown]
+# We now take a look at the help for the `piqtree_phylo` app.
+
+# %%
+
+from cogent3 import app_help
+
+app_help("piqtree_phylo")
+
+# %% [markdown]
+# We build an app for estimating phylogenies with a GTR model
+
+# %%
+from cogent3 import get_app
+
+phylo_gtr = get_app("piqtree_phylo", "GTR", bootstrap_reps=1000)
+phylo_gtr
+
+# %% [markdown]
+# ## Run the phylogeny estimation and display branches with support < 90%.
+
+# %%
+tree = phylo_gtr(aln)
+dnd = tree.get_figure(show_support=True, threshold=90)
+dnd.show(width=600, height=600)
