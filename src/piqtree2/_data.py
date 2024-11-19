fb6f102c89caac0b4a87898946174baf8a2ff3a3
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-11-20 09:01:33 +1100

    ENH: add support for downloading sample data sets
    [NEW] piqtree.dataset_names() and piqtree.download_dataset()
        functions to list and download sample data sets. The data sets are
        stored in this GitHub issue https://github.com/iqtree/piqtree2/issues/98.
    
        These functions used in the documentation.

---
+++ src/piqtree2/_data.py
@@ -0,0 +1,72 @@
+import pathlib
+import zipfile
+
+import requests
+
+_data_files = {
+    "mammal-orths.zip": "https://github.com/user-attachments/files/17806562/mammal-orths.zip",
+    "brca1.fasta.gz": "https://github.com/user-attachments/files/17806563/brca1.fasta.gz",
+    "example.phy.gz": "https://github.com/user-attachments/files/17806561/example.phy.gz",
+    "example.tree.gz": "https://github.com/user-attachments/files/17821150/example.tree.gz",
+}
+
+
+def _inflate_zip(zip_path: str, output_dir: str) -> str:
+    """decompress the contents of a zip file to a named directory"""
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(output_dir)
+    return output_dir
+
+
+def _get_url(name: str) -> str:
+    """URL for a data file"""
+    if name not in _data_files:
+        raise ValueError(f"Unknown data file: {name}")
+    return _data_files[name]
+
+
+def dataset_names():
+    """return the names of available datasets"""
+    return list(_data_files.keys())
+
+
+def download_dataset(
+    name: str, dest_dir: str, dest_name: str | None = None, inflate_zip: bool = True
+) -> pathlib.Path:
+    """download a data files used in docs, requires an internet connection
+
+    Parameters
+    ----------
+    name
+        data set name, see `dataset_names()`
+    dest_dir
+        where to write a local copy
+    dest_name
+        name of the file to write, if None uses name
+    inflate_zip
+        unzip archives
+
+    Returns
+    -------
+    path to the downloaded file
+
+    Notes
+    -----
+    Only downloads if dest_dir / dest_name does not exist.
+    """
+    url = _get_url(name)
+    dest_name = dest_name or name
+    outpath = pathlib.Path(dest_dir) / dest_name
+    outpath.parent.mkdir(parents=True, exist_ok=True)
+    if outpath.exists():
+        return outpath
+
+    response = requests.get(url, stream=True, timeout=20)
+    block_size = 4096
+    with outpath.open("wb") as out:
+        for data in response.iter_content(block_size):
+            out.write(data)
+
+    if inflate_zip and outpath.suffix == ".zip":
+        outpath = _inflate_zip(outpath, dest_dir)
+    return outpath
