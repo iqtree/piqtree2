0e67c1debe177f82505d306eb603e848f9a4e98d
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-20 03:20:33 +0000

    DEV: add nox linting

--- noxfile.py
+++ noxfile.py
@@ -23,3 +23,13 @@ def type_check(session):
     install_spec = ".[typing]"
     session.install(install_spec)
     session.run("mypy", "src", "tests", *posargs, env=env)
+
+
+@nox.session(python=[f"3.{v}" for v in _py_versions])
+def ruff(session):
+    posargs = list(session.posargs)
+    env = os.environ.copy()
+
+    install_spec = ".[lint]"
+    session.install(install_spec)
+    session.run("ruff", "check", *posargs, env=env)
