3651e16df2578bff5f7a75127123b7f7156f975b
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-20 00:36:09 +0000

    MAINT: resolve typing issues

--- src/piqtree2/_data.py
+++ src/piqtree2/_data.py
@@ -11,7 +11,7 @@ _data_files = {
 }
 
 
-def _inflate_zip(zip_path: str, output_dir: str) -> str:
+def _inflate_zip(zip_path: pathlib.Path, output_dir: pathlib.Path) -> pathlib.Path:
     """decompress the contents of a zip file to a named directory"""
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(output_dir)
@@ -25,13 +25,16 @@ def _get_url(name: str) -> str:
     return _data_files[name]
 
 
-def dataset_names():
+def dataset_names() -> list[str]:
     """return the names of available datasets"""
     return list(_data_files.keys())
 
 
 def download_dataset(
-    name: str, dest_dir: str, dest_name: str | None = None, inflate_zip: bool = True
+    name: str,
+    dest_dir: str | pathlib.Path,
+    dest_name: str | None = None,
+    inflate_zip: bool = True,
 ) -> pathlib.Path:
     """download a data files used in docs, requires an internet connection
 
@@ -54,9 +57,11 @@ def download_dataset(
     -----
     Only downloads if dest_dir / dest_name does not exist.
     """
+    dest_dir = pathlib.Path(dest_dir)
+
     url = _get_url(name)
     dest_name = dest_name or name
-    outpath = pathlib.Path(dest_dir) / dest_name
+    outpath = dest_dir / dest_name
     outpath.parent.mkdir(parents=True, exist_ok=True)
     if outpath.exists():
         return outpath
