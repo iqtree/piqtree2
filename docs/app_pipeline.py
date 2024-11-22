0eb71a395136ee34bef46134ab93fddad0cdfc97
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-11-22 18:20:03 +1100

    DOC: fixed linting failure

--- docs/app_pipeline.py
+++ docs/app_pipeline.py
@@ -2,7 +2,7 @@
 # ## Building workflows using `piqtree2` apps
 # > **WARNING**
 # > This page is under construction!
-# 
+#
 # We can combine `piqtree2` apps with other cogent3 apps to develop a pipeline. There are multiple concepts involved here, particularly data stores, composed apps, parallel execution, log files etc... See the cogent3 [app documentation](https://cogent3.org/doc/app/index.html) for more details.
 #
 # To develop a pipeline efficiently we only need a subset of the sequences in an alignment. We will use the [diverse-seq](https://pypi.org/project/diverse-seq/) plugin for that purpose. This allows selecting a specified subset of sequences that capture the diversity in an alignment.
