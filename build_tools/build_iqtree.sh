7e95e403775c9502bb2626626f8bab6e04df7977
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-29 04:19:41 +0000

    DEV: remove single core build flag

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -1,7 +1,7 @@
 cd iqtree2
 rm -rf build
 mkdir build && cd build
-cmake -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
+cmake -DBUILD_LIB=ON ..
 make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
