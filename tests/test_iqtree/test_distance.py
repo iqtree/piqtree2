866244140357525f4589d517bd93b7df6401b272
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-12-02 03:46:36 +0000

    TST: ensure distances are not zero

--- tests/test_iqtree/test_distance.py
+++ tests/test_iqtree/test_distance.py
@@ -7,22 +7,22 @@ def test_jc_distance(five_otu: ArrayAlignment) -> None:
     dists = jc_distances(five_otu)
 
     assert (
-        dists["Human", "Chimpanzee"] < dists["Human", "Dugong"]
+        0 < dists["Human", "Chimpanzee"] < dists["Human", "Dugong"]
     )  # chimpanzee closer than rhesus
     assert (
-        dists["Human", "Rhesus"] < dists["Human", "Manatee"]
+        0 < dists["Human", "Rhesus"] < dists["Human", "Manatee"]
     )  # rhesus closer than manatee
     assert (
-        dists["Human", "Rhesus"] < dists["Human", "Dugong"]
+        0 < dists["Human", "Rhesus"] < dists["Human", "Dugong"]
     )  # rhesus closer than dugong
 
     assert (
-        dists["Chimpanzee", "Rhesus"] < dists["Chimpanzee", "Manatee"]
+        0 < dists["Chimpanzee", "Rhesus"] < dists["Chimpanzee", "Manatee"]
     )  # rhesus closer than manatee
     assert (
-        dists["Chimpanzee", "Rhesus"] < dists["Chimpanzee", "Dugong"]
+        0 < dists["Chimpanzee", "Rhesus"] < dists["Chimpanzee", "Dugong"]
     )  # rhesus closer than dugong
 
     assert (
-        dists["Manatee", "Dugong"] < dists["Manatee", "Rhesus"]
+        0 < dists["Manatee", "Dugong"] < dists["Manatee", "Rhesus"]
     )  # dugong closer than rhesus
