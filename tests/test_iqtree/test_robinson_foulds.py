74e0e5d90741804d1ab3550efe8af04f054d5d2d
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-12-12 03:12:19 +0000

    API: change API for `robinson_foulds` to accept a sequence of trees

--- tests/test_iqtree/test_robinson_foulds.py
+++ tests/test_iqtree/test_robinson_foulds.py
@@ -7,5 +7,5 @@ import piqtree2
 def test_robinson_foulds() -> None:
     tree1 = "(A,B,(C,D));"
     tree2 = "(A,C,(B,D));"
-    pairwise_distances = piqtree2.robinson_foulds(tree1, tree2)
+    pairwise_distances = piqtree2.robinson_foulds([tree1, tree2])
     assert_array_equal(pairwise_distances, np.array([[0, 2], [2, 0]]))
