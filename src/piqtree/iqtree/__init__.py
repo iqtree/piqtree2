d3764004e324ed72bb078602392623472cc4a452
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-12-13 04:23:30 +0000

    TST: extend tests for `model_finder`

--- src/piqtree/iqtree/__init__.py
+++ src/piqtree/iqtree/__init__.py
@@ -1,13 +1,14 @@
 """Functions for calling IQ-TREE as a library."""
 
 from ._jc_distance import jc_distances
-from ._model_finder import ModelFinderResult, model_finder
+from ._model_finder import ModelFinderResult, ModelResultValue, model_finder
 from ._random_tree import TreeGenMode, random_trees
 from ._robinson_foulds import robinson_foulds
 from ._tree import build_tree, fit_tree, nj_tree
 
 __all__ = [
     "ModelFinderResult",
+    "ModelResultValue",
     "TreeGenMode",
     "build_tree",
     "fit_tree",
